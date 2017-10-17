417dd0c1c5f3b3c681028c8ca28f0f80f6c35618
Kevin Grozis <kevingrozis@juniper.net>
2017-10-17 13:33:58 -0400

    - hello world

---
+++ 01__Tools-and-Framework-Options/hello.js
@@ -0,0 +1,7 @@
+"use strict";
+// Execute with tsc hello.ts
+// Generates file hell.js
+// tsc --init creates tsconfig.json
+// when tsconfig.json is created.  can just type tsc from dir to execute
+// can create multiple tsconfig.json files per dir
+console.log('hello TypeScript');
---
+++ 01__Tools-and-Framework-Options/hello.ts
@@ -0,0 +1,7 @@
+// Execute with 'tsc hello.ts'
+// Generates file hello.js
+// 'tsc --init' creates tsconfig.json
+// when 'tsconfig.json' is created.  can just type 'tsc' from dir to execute
+// can create multiple 'tsconfig.json' files per dir
+// can run with 'node hello.js'
+console.log('hello TypeScript');
